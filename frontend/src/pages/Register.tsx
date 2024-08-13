d64eacf353877dfb96a14d9d7da53920e3a7e74d
samuelalves3324 <samucahhh@gmail.com>
2024-08-12 23:28:59 -0300

    Feat: Create register form

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,10 @@
+import RegisterForm from "../components/RegisterForm";
+
 export default function Register() {
   return(
-    <></>
+    <main>
+      <h1>Register</h1>
+      <RegisterForm />
+    </main>
   );
 };
