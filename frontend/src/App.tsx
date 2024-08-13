3b86442ef6fb9e6e766afcc44529526a437e6dfc
samuelalves3324 <samucahhh@gmail.com>
2024-08-12 22:26:05 -0300

    Feat: Create login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 
 export default function App() {
   return(
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/auth/login" />
+          <Route path="/auth/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
