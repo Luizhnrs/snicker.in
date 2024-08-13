29bc336ec1abdb4b1f932d7b841fe92ad9a5c849
samuelalves3324 <samucahhh@gmail.com>
2024-08-12 23:50:01 -0300

    Feat: Styling register page

--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -1,36 +1,40 @@
+import { Link } from "react-router-dom";
+
 export default function RegisterForm() {
   return (
-    <div>
-      <form>
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form className="space-y-6">
 
         <div>
-          <label htmlFor="name">Nome Completo</label>
-          <div>
+          <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Nome Completo</label>
+          <div className="mt-2">
             <input
               type="text"
               id="name"
               name="name"
               required
               autoComplete="name"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
           </div>
         </div>
 
         <div>
-          <label htmlFor="email">Email</label>
-          <div>
+          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email</label>
+          <div className="mt-2">
             <input
               type="email"
               id="email"
               name="email"
               required
               autoComplete="email"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
           </div>
         </div>
 
         <div>
-          <label htmlFor="phoneNumber">Nuḿero de Telefone</label>
+          <label htmlFor="phoneNumber" className="block text-sm font-medium leading-6 text-gray-900">Nuḿero de Telefone</label>
           <div>
             <input
               type="text"
@@ -38,30 +42,33 @@ export default function RegisterForm() {
               name="phoneNumber"
               required
               autoComplete="tel"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
           </div>
         </div>
 
         <div>
-          <label htmlFor="cpf">CPF</label>
-          <div>
+          <label htmlFor="cpf" className="block text-sm font-medium leading-6 text-gray-900">CPF</label>
+          <div className="mt-2">
             <input
               type="text"
               id="cpf"
               name="cpf"
               required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
           </div>
         </div>
 
         <div>
-          <label htmlFor="password">Senha</label>
-          <div>
+          <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Senha</label>
+          <div className="mt-2">
             <input
               type="password"
               id="password"
               name="password"
               required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
           </div>
         </div>
@@ -69,8 +76,16 @@ export default function RegisterForm() {
         <div>
           <button
           type="submit"
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >Criar Conta</button>
         </div>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+            Já tem uma conta?{' '}
+            <Link to="/auth/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Fazer login
+            </Link>
+          </p>
 
       </form>
     </div>
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,9 +1,11 @@
 import RegisterForm from "../components/RegisterForm";
 
 export default function Register() {
-  return(
-    <main>
-      <h1>Register</h1>
+  return (
+    <main className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register</h1>
+      </div>
       <RegisterForm />
     </main>
   );
