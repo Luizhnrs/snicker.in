c1e67b8e2dfa21ab4796324d82ef297c926a73aa
samuelalves3324 <samucahhh@gmail.com>
2024-08-12 23:44:47 -0300

    Feat: Add form labels and inputs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import "./styles/output.css"
+import Register from "./pages/Register";
 
 export default function App() {
   return(
@@ -8,6 +9,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -1,5 +1,78 @@
 export default function RegisterForm() {
-  return(
-    <></>
+  return (
+    <div>
+      <form>
+
+        <div>
+          <label htmlFor="name">Nome Completo</label>
+          <div>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+              autoComplete="name"
+            />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <div>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              autoComplete="email"
+            />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="phoneNumber">Nuḿero de Telefone</label>
+          <div>
+            <input
+              type="text"
+              id="phoneNumber"
+              name="phoneNumber"
+              required
+              autoComplete="tel"
+            />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="cpf">CPF</label>
+          <div>
+            <input
+              type="text"
+              id="cpf"
+              name="cpf"
+              required
+            />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="password">Senha</label>
+          <div>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+          type="submit"
+          >Criar Conta</button>
+        </div>
+
+      </form>
+    </div>
   )
 };
