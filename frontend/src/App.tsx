6982011ed75c0f94bfd5320a25b69c5aa94ebb91
samuelalves3324 <samucahhh@gmail.com>
2024-08-13 00:07:10 -0300

    Feat: Create home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import "./styles/output.css"
 import Register from "./pages/Register";
+import Home from "./pages/Home";
 
 export default function App() {
   return(
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/auth/login" element={<Login />} />
           <Route path="/auth/register" element={<Register />} />
         </Routes>
