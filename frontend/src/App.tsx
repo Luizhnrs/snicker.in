548dfd4803552ab0b1d0670b248876ac3b34178a
samuelalves3324 <samucahhh@gmail.com>
2024-08-12 23:19:57 -0300

    Feat: Move input and output css to styles directory

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
-import "./output.css"
+import "./styles/output.css"
 
 export default function App() {
   return(
