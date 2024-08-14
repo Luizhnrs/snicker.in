ae1129584a3c8d722b6913e2c90583109a15f335
luizhnrs <luizhnrsg@gmail.com>
2024-08-13 21:07:40 -0300

    Application Properties

---
+++ snikerin/src/main/java/com/example/snikerin/SnikerinApplication.java
@@ -0,0 +1,13 @@
+package com.example.snikerin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnikerinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnikerinApplication.class, args);
+	}
+
+}
---
+++ snikerin/src/test/java/com/example/snikerin/SnikerinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snikerin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnikerinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
