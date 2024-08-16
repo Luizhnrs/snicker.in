7b606504b14e2c17c7a6b71ea353e3e5a795f24f
samuelalves3324 <samucahhh@gmail.com>
2024-08-16 12:25:27 -0300

    Feat: Styling login page

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -7,31 +7,34 @@ type LoginFormProps = {
 
 export default function LoginForm({ email, setEmail, password, setPassword }: LoginFormProps) {
   return (
-    <form className="space-y-6">
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        required
-        value={email}
-        onChange={({ target }) => setEmail(target.value)}
-        autoComplete="email" />
-
-      <label htmlFor="password">Senha</label>
-      <input
-        type="password"
-        name="password"
-        id="password"
-        required
-        value={password}
-        onChange={({ target }) => setPassword(target.value)}
-        autoComplete="current-password"
-      />
+    <form>
+      <div className="input-div">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+          autoComplete="email" />
+      </div>
+      <div className="input-div">
+        <label htmlFor="password">Senha</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          required
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+          autoComplete="current-password"
+        />
+      </div>
       <button
         type="submit"
+        className="login-form-button"
       >Entrar</button>
-
     </form>
   );
 };
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,19 +1,18 @@
 import { Link } from "react-router-dom";
 import LoginForm from "../components/LoginForm";
 import { useState } from "react";
+import "../styles/login.css";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <main>
-      <div>
-        <h1>Login</h1>
-        <LoginForm email={email} setEmail={setEmail} password={password} setPassword={setPassword} />
-      </div>
-      <p>
+    <main className="login-container">
+      <h1>Login</h1>
+      <LoginForm email={email} setEmail={setEmail} password={password} setPassword={setPassword} />
+      <p className="login-message">
         Não tem uma conta?{' '}
-        <Link to={"/auth/register"}>
+        <Link to={"/auth/register"} className="register-link">
           Criar conta
         </Link>
       </p>
