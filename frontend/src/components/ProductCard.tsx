00a45f76053846b92e9719a0826507f7c00ed3fe
samuelalves3324 <samucahhh@gmail.com>
2024-08-19 08:51:18 -0300

    Feat: Styling product card

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,5 +1,28 @@
-export default function ProductCard() {
-  return(
-    <li></li>
+import { Link } from "react-router-dom";
+import "../styles/productCard.css"
+
+type ProductCardProps = {
+  product: {
+    id: number,
+    name: string,
+    img: string,
+    price: number,
+    brand: string,
+    onSale: boolean,
+    salePrice: number,
+    color: string,
+  },
+};
+
+export default function ProductCard({ product }: ProductCardProps) {
+  const { id, name, img, price, brand, onSale, salePrice, color } = product;
+  return (
+    <li className="product-card">
+      <Link to={`/product/${id}`}>
+        <img src={img} alt={`${brand} ${name} image`} />
+        <p>{brand} - {name}</p>
+        { onSale ? <p>R$ {salePrice.toFixed(2)} <span className="no-sale-price">R$ {price.toFixed(2)}</span></p> : <p>R$ {price.toFixed(2)}</p> }
+      </Link>
+    </li>
   );
 };
