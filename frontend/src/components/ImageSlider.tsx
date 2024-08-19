cc1ad289c94c77a59d12fd3ad8a6f192da52aa3c
samuelalves3324 <samucahhh@gmail.com>
2024-08-19 16:25:06 -0300

    Fix: Fix imagem slider so that the image changes when you hover the mouse over the thumbnail

--- frontend/src/components/ImageSlider.tsx
+++ frontend/src/components/ImageSlider.tsx
@@ -8,29 +8,15 @@ type ImageSliderProps = {
 const ImageSlider = ({ images }: ImageSliderProps) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const goToPrevious = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const goToNext = () => {
-    const isLastSlide = currentIndex === images.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const goToSlide = (index: number) => {
+  const handleMouseEnter = (index: number) => {
     setCurrentIndex(index);
   };
 
   return (
     <div className="slider">
-      <button onClick={goToPrevious} className="left-arrow">❮</button>
       <div className="slider-content">
         <img src={images[currentIndex]} alt="slide" className="slide-image" />
       </div>
-      <button onClick={goToNext} className="right-arrow">❯</button>
       
       <div className="thumbnails">
         {images.map((image, index) => (
@@ -39,7 +25,7 @@ const ImageSlider = ({ images }: ImageSliderProps) => {
             src={image}
             alt={`Thumbnail ${index + 1}`}
             className={`thumbnail ${currentIndex === index ? 'active' : ''}`}
-            onClick={() => goToSlide(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
           />
         ))}
       </div>
