ecfa9f78e5cd8238af655247db89c26215dea52f
samuelalves3324 <samucahhh@gmail.com>
2024-08-19 10:01:46 -0300

    Feat: Create product page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import Product from "./pages/Product";
 
 export default function App() {
   return(
@@ -12,6 +13,7 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/auth/login" element={<Login />} />
           <Route path="/auth/register" element={<Register />} />
+          <Route path="/product/:productId" element={<Product />} />
           <Route path="/products/:type" element={<Products />} />
         </Routes>
       </BrowserRouter>
---
+++ frontend/src/pages/Product.tsx
@@ -0,0 +1,16 @@
+const product = {
+  id: 1,
+  name: "Dunk low",
+  img: "https://www.copclub.com.br/cdn/shop/products/Wethenew-Sneakers-France-Nike-Dunk-Low-University-Blue-DD1391-102-1.0_800x_81d4aafa-9f95-4f99-8532-1ea4dcb214b0_grande.png?v=1622127347",
+  price: 1044.99,
+  brand: "Nike",
+  onSale: false,
+  salePrice: 0,
+  color: "blue",
+};
+
+export default function Product() {
+  return(
+    <>Product</>
+  );
+};
