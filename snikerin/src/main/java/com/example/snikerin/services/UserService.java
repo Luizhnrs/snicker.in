df2e88362e5a663581067cc0f772b35a08cbd8f6
luizhnrs <luizhnrsg@gmail.com>
2024-08-19 16:57:58 -0300

    Feat: User service implemented

--- snikerin/src/main/java/com/example/snikerin/services/UserService.java
+++ snikerin/src/main/java/com/example/snikerin/services/UserService.java
@@ -1,9 +1,14 @@
 package com.example.snikerin.services;
 
 import com.example.snikerin.dtos.CreateUserDto;
+import com.example.snikerin.exceptions.UserNotFoundException;
 import com.example.snikerin.models.User;
 import com.example.snikerin.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -13,9 +18,30 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User createUser(CreateUserDto dto) {
-        User user = new User();
+    public User createUser(User user, CreateUserDto dto) {
         return userRepository.save(user);
     }
 
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(UUID id, User user) throws UserNotFoundException {
+     return userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+    }
+
+    public User updateUser(UUID id, User user) throws UserNotFoundException {
+        User userSearched = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+        userSearched.setFirstName(user.getFirstName());
+        userSearched.setLastName(user.getLastName());
+        userSearched.setCpfCnpj(user.getCpfCnpj());
+        userSearched.setEmail(user.getEmail());
+        userSearched.setPassword(user.getPassword());
+        return userRepository.save(userSearched);
+    }
+
+    public void deleteUser(UUID id) throws UserNotFoundException {
+        User userToDelete = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+        userRepository.delete(userToDelete);
+    }
 }
