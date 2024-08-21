86614670e998ebff33b46755f2822f6455a8bb40
samuelalves3324 <samucahhh@gmail.com>
2024-08-21 00:30:25 -0300

    Feat: Render cart products

--- frontend/src/components/CartTable.tsx
+++ frontend/src/components/CartTable.tsx
@@ -1,31 +1,13 @@
 import React from 'react';
 import '../styles/cartTable.css';
 import trashIcon from '../assets/trash.svg';
+import {CartProductType} from '../types/CartProductType';
 
-const products = [
-  {
-    id: 1,
-    name: 'Dunk low',
-    img: 'https://www.copclub.com.br/cdn/shop/products/Wethenew-Sneakers-France-Nike-Dunk-Low-University-Blue-DD1391-102-1.0_800x_81d4aafa-9f95-4f99-8532-1ea4dcb214b0_grande.png?v=1622127347',
-    price: 1044.99,
-    brand: 'Nike',
-    onSale: false,
-    salePrice: 0,
-    color: 'blue',
-  },
-  {
-    id: 2,
-    name: 'Air jordan',
-    img: 'https://www.copclub.com.br/cdn/shop/files/jordan-w-air-jordan-1-low-panda_20032224_45173127_2048_1200x.png?v=1685563664',
-    price: 845.90,
-    brand: 'Nike',
-    onSale: true,
-    salePrice: 789.99,
-    color: 'white',
-  },
-];
+type CartTableProps = {
+  products: CartProductType[],
+}
 
-export default function CartTable() {
+export default function CartTable({products}: CartTableProps) {
   return (
     <table className="cart-table">
       <thead>
--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -5,13 +5,15 @@ import ShippingEstimateForm from '../components/ShippingEstimateForm';
 import '../styles/cart.css';
 import DiscountCouponForm from '../components/DiscountCouponForm';
 import CartSummaryTable from '../components/CartSummaryTable';
+import {useCart} from '../contexts/CartContext';
 
 export default function Cart() {
+  const {cartProducts} = useCart();
   return (
     <main className="cart-page">
       <Header />
       <div className="cart-container">
-        <CartTable />
+        <CartTable products={cartProducts} />
         <div className="cart-checkout">
           <ShippingEstimateForm />
           <DiscountCouponForm />
