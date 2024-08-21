1ef45807b8a49bb24fae507992f295e92ca8d430
samuelalves3324 <samucahhh@gmail.com>
2024-08-20 21:45:00 -0300

    Feat: Create shipping estimate form

---
+++ frontend/src/components/ShippingEstimateForm.tsx
@@ -0,0 +1,9 @@
+export default function ShippingEstimateForm() {
+  return (
+    <form>
+      <p>Prazo de entrega</p>
+      <input type="text" id="cep" />
+      <button>Calcular</button>
+    </form>
+  );
+};
--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,5 +1,6 @@
 import CartTable from "../components/CartTable";
 import Header from "../components/Header"
+import ShippingEstimateForm from "../components/ShippingEstimateForm";
 import "../styles/cart.css";
 
 export default function Cart() {
@@ -8,12 +9,39 @@ export default function Cart() {
       <Header />
       <div className="cart-container">
         <CartTable />
-        <div>
-          <p>Cupom de desconto</p>
+        <div className="div">
+          <ShippingEstimateForm />
           <form>
+            <p>Cupom de desconto</p>
             <input type="text" id="cupon" />
             <button>Aplicar</button>
           </form>
+          <div>
+            <p>Resumo</p>
+            <table>
+              <hr />
+              <tr>
+                <th>Valor dos produtos</th>
+                <td>R$</td>
+              </tr>
+              <hr />
+              <tr>
+                <th>Frete</th>
+                <td>Grátis</td>
+              </tr>
+              <hr />
+              <tr>
+                <th>Descontos</th>
+                <td> - R$</td>
+              </tr>
+              <hr />
+              <tr>
+                <th>Total das compras</th>
+                <td>R$</td>
+              </tr>
+            </table>
+            <button>Continuar para o pagamento</button>
+          </div>
         </div>
       </div>
     </main>
