0a52fc5640955e5e2182ecd1627d6562110554be
samuelalves3324 <samucahhh@gmail.com>
2024-08-22 13:01:13 -0300

    Feat: Add transiction in burguer menu

--- frontend/src/components/BurguerMenu/BurguerMenu.tsx
+++ frontend/src/components/BurguerMenu/BurguerMenu.tsx
@@ -1,68 +1,61 @@
-import React from 'react';
-import {useState} from 'react';
-import burguerMenuIcon from '../../assets/burguerMenu.svg';
-import './burguerMenu.css';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import burguerMenuIcon from '../../assets/burguerMenu.svg';
+import arrowIcon from '../../assets/arrow.svg';
+import './burguerMenu.css';
 
 export default function BurguerMenu() {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const openMenu = () => {
     setOpen(!open);
   };
+
   return (
     <div className="burguer-menu">
       <button onClick={openMenu} className="burguer-menu-button">
         <img src={burguerMenuIcon} alt="Burguer Menu Icon" />
       </button>
-      {
-        open && (
-          <ul className="burguer-menu-list">
-            <li>
-              <Link
-                to="/products/footwear"
-                className="burguer-menu-list-link">
-                <p>Calçados</p>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/products/clothes"
-                className="burguer-menu-list-link"
-              >
-                <p>Roupas</p>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/products/accessories"
-                className="burguer-menu-list-link">
-                <p>Acessórios</p>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/cart"
-                className="burguer-menu-list-link">
-                <p>Carrinho</p>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/auth/login"
-                className="burguer-menu-list-link">
-                <p>Entrar</p>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/auth/register"
-                className="burguer-menu-list-link">
-                <p>Criar conta</p>
-              </Link>
-            </li>
-          </ul>
-        )
-      }
+      <ul className={`burguer-menu-list ${open ? 'open' : ''}`}>
+        <hr />
+        <div className="burguer-menu-categories">
+          <li>
+            <Link to="/products/footwear" onClick={openMenu}>
+              <p>CALÇADOS</p>
+              <img src={arrowIcon} alt="Arrow Icon" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/products/clothes" onClick={openMenu}>
+              <p>ROUPAS</p>
+              <img src={arrowIcon} alt="Arrow Icon" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/products/accessories" onClick={openMenu}>
+              <p>ACESSÓRIOS</p>
+              <img src={arrowIcon} alt="Arrow Icon" />
+            </Link>
+          </li>
+        </div>
+        <hr />
+        <div className="burguer-menu-utils">
+          <li>
+            <Link to="/cart">
+              <p>Carrinho</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/auth/login">
+              <p>Entrar</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/auth/register">
+              <p>Criar conta</p>
+            </Link>
+          </li>
+        </div>
+      </ul>
     </div>
   );
 }
