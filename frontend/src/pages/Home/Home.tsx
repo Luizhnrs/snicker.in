156b3ec2b684a4885b1e5fed8f74d73f071582e5
samuelalves3324 <samucahhh@gmail.com>
2024-08-27 22:14:08 -0300

    Feat: Create products carousel

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Header from '../../components/Header';
 import './home.css';
+import ProductCarousel from '../../components/ProductCarousel/ProductsCarousel';
 
 export default function Home() {
   return (
     <main className="home-page">
       <Header />
       <div className="home-container">
-        <img src="https://images6.alphacoders.com/133/1331511.png" alt="" />
+        <img src="https://assets.propmark.com.br/uploads/2022/07/unnamed---2022-07-07T095945.233.jpg" alt="" />
+        <div>
+          <h3>Mais populares</h3>
+          <ProductCarousel />
+        </div>
+        <div>
+          <h3>Na promoção</h3>
+        </div>
       </div>
     </main>
   );
