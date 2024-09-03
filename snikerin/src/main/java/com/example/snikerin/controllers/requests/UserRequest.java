c3851dbd0f151f08a24cd846bffb3cacf8447bcb
samuelalves3324 <samucahhh@gmail.com>
2024-09-03 20:52:39 -0300

    Feat: Update UserRequest to align with User entity changes (cpf instead of cpfCnpj)

--- snikerin/src/main/java/com/example/snikerin/controllers/requests/UserRequest.java
+++ snikerin/src/main/java/com/example/snikerin/controllers/requests/UserRequest.java
@@ -3,13 +3,13 @@ package com.example.snikerin.controllers.requests;
 import com.example.snikerin.models.User;
 
 public record UserRequest(
-       String firstName,
-       String lastName,
-       String cpfCnpj,
-       String email,
-       String password
+        String firstName,
+        String lastName,
+        String cpf,
+        String email,
+        String password
 ) {
     public User toEntity(){
-        return new User(firstName, lastName, cpfCnpj, email, password);
+        return new User(firstName, lastName, cpf, email, password);
     }
 }
